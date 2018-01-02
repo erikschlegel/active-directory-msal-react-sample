a0aae19f96ec46c9de381f75df0b0229cf769829
Erik Schlegel <erisch@microsoft.com>
2018-01-02 10:29:50 -0600

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MSAL from './Auth';
+
+function LoginButton(props) {
+  return (
+    <a className="btn btn-primary" onClick={props.onClick}>
+      Login
+    </a>
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <a className="btn btn-primary" onClick={props.onClick}>
+      Logout {props.username}
+    </a>
+  );
+}
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+
+    this.state = {
+      authProfile: new MSAL().init(),
+      isLoggedIn: false
+    };
+  }
+
+  handleLogoutClick(){
+    this.state.authProfile.adLogout().then(()=>{
+      this.setState({isLoggedIn: false});
+    });
+  }
+
+  handleLoginClick(){
+    const self = this;
+    this.state.authProfile.adLogin().then(()=>{
+      self.setState({isLoggedIn: true});
+    });
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+
+    let button = null;
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} 
+                             username={this.state.authProfile.getUsername()} />;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />;
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+        {button}
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Auth.js
@@ -0,0 +1,76 @@
+import { UserAgentApplication, Logger } from 'msal';
+import Promise from 'promise';
+
+const adAppId = process.env.REACT_APP_AD_CLIENT_ID;
+const adGraphScope = ["user.read", "mail.send"];
+const TokenStoreKey = 'AD.Token';
+
+function adHandleToken(token, resolve) {
+    console.log(`called ${token}`);
+    localStorage.setItem(TokenStoreKey, token);
+    resolve();
+}
+
+function adHandleError(error) {
+    console.error(`AD: ${error}`);
+}
+
+function authCallback(errorMessage, token, error, tokenType) {
+    if (!errorMessage && token) {
+        console.log('received token');
+    } else {
+        this.adHandleError(error);
+    }
+}
+
+export default class AuthClient {
+    init() {
+        this.adApp = new UserAgentApplication(
+            adAppId,
+            null,
+            authCallback,
+            {
+                cacheLocation: 'localStorage',
+                logger: new Logger((level, message, containsPII) => {
+                    const logger = level === 0 ? console.error : level === 1 ? console.warn : console.log;
+                    logger(`AD: ${message}`);
+                })
+            }
+        )
+
+        return this;
+    }
+    adLogin(callback) {
+        return new Promise((resolve, reject) => {
+            if (this.adApp) {
+                this.adApp.loginPopup(adGraphScope).then(idToken => {
+                    this.adApp.acquireTokenSilent(adGraphScope)
+                        .then(token=>adHandleToken(token, resolve))
+                        .catch(error => {
+                            this.adApp.acquireTokenPopup(adGraphScope)
+                                .then(token=>adHandleToken(token, resolve))
+                                .catch(adHandleError);
+                        });
+                }).catch(adHandleError);
+            } else {
+                reject('trying to call adLogin before authclient is initialized');
+            }
+        });
+    }
+    getUsername() {
+        return this.adApp.getUser().name;
+    }
+    isLoggedIn() {
+        return this.adApp.getUser() && this.adApp.getUser().name && this.getToken();
+    }
+    getToken() {
+        return localStorage.getItem(TokenStoreKey);
+    }
+    adLogout() {
+        return new Promise((resolve, reject) => {
+            this.adApp.logout();
+            localStorage.removeItem(TokenStoreKey);
+            resolve();
+        }).catch(adHandleError);
+    }
+}
